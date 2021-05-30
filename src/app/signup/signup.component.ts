e031473bc5d8d88328e619cd8008e1220f8f8beb
Matus <matusino.sykora@gmail.com>
2021-05-30 12:53:35 +0200

    sign-up component refactor

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SignupRequestPayload } from './signup-request.payload';
-import {AuthService} from '../shared/auth.service';
+import { AuthService } from '../shared/auth.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
@@ -14,36 +14,40 @@ export class SignupComponent implements OnInit {
 
   signupRequestPayload: SignupRequestPayload;
   signupForm: FormGroup;
+  isSubmitted = false;
 
-  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) { 
+  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) {
     this.signupRequestPayload = {
-      username:'',
-      email:'',
-      password:''
+      username: '',
+      email: '',
+      password: ''
     }
   }
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required,Validators.email]),
-      password: new FormControl('', Validators.required)
+      username: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
 
     })
   }
+  get formControls() {
+    return this.signupForm.controls;
+  }
 
-  signup(){
+  signup() {
     this.signupRequestPayload.email = this.signupForm.get('email').value;
     this.signupRequestPayload.username = this.signupForm.get('username').value;
     this.signupRequestPayload.password = this.signupForm.get('password').value;
 
     this.authService.signup(this.signupRequestPayload)
       .subscribe(data => {
-        this.router.navigate(['/login'],
+        this.router.navigate(['/'],
           { queryParams: { registered: 'true' } });
       }, error => {
         console.log(error);
-        this.toastr.error('Registration Failed! Please try again');
+        this.toastr.error('Registration Failed!');
       });
   }
 }
