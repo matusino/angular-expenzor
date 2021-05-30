8164c18ab691a61e6647173ce2abf1e065a88843
Matus <matusino.sykora@gmail.com>
2021-05-30 12:56:45 +0200

    header - login button added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from 'ngx-webstorage';
+import { BehaviorSubject, from, Observable } from 'rxjs';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean;
+  username: string;
+
+  constructor(public authService: AuthService, private localStorage: LocalStorageService, private router: Router) {
+
+  }
 
   ngOnInit(): void {
+
+    this.authService.loggedIn.subscribe((data: boolean) => this.isLoggedIn = data);
+    this.authService.username.subscribe((data: string) => this.username = data);
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.username = this.authService.getUserName();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.router.navigateByUrl('/');
   }
 
 }
