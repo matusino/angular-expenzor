bb663052c11fa19cf78761cd866e756549625d00
Matus <matusino.sykora@gmail.com>
2021-05-30 12:55:48 +0200

    login refactor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LoginRequestPayload } from './login-request.payload';
-import {AuthService} from '../shared/auth.service';
+import { AuthService } from '../shared/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
@@ -39,13 +39,13 @@ export class LoginComponent implements OnInit {
       });
   }
 
-  login(){
+  login() {
     this.loginRequestPayload.username = this.loginForm.get('username').value;
     this.loginRequestPayload.password = this.loginForm.get('password').value;
 
     this.authService.login(this.loginRequestPayload).subscribe(data => {
       this.isError = false;
-      this.router.navigateByUrl('/');
+      this.router.navigateByUrl('/home');
       this.toastr.success('Login Successful');
     }, error => {
       this.isError = true;
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpXsrfTokenExtractor } from '@angular/common/http';
 import { EventEmitter, Injectable, Output } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { SignupRequestPayload } from '../signup/signup-request.payload';
 import { LoginRequestPayload } from '../login/login-request.payload';
 import { LoginResponse } from '../login/login-response.payload';
@@ -14,37 +14,55 @@ export class AuthService {
 
   @Output() loggedIn: EventEmitter<boolean> = new EventEmitter();
   @Output() username: EventEmitter<string> = new EventEmitter();
-  
 
-  constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) {
+  isLoggedin: boolean = false;
 
-   }
 
-   signup(signupRequestPayload: SignupRequestPayload): Observable<any>{
-     return this.httpClient.post('http://localhost:8080/auth/signup', signupRequestPayload, {responseType:'text'});
-   }
+  constructor(private httpClient: HttpClient, private localStorage: LocalStorageService, private tokenExtractor: HttpXsrfTokenExtractor) { }
 
-   login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
+  signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
+    return this.httpClient.post('http://localhost:8080/auth/registration', signupRequestPayload, { responseType: 'text' });
+  }
+
+  login(loginRequestPayload: LoginRequestPayload): Observable<any> {
     return this.httpClient.post<LoginResponse>('http://localhost:8080/auth/login',
       loginRequestPayload).pipe(map(data => {
         this.localStorage.store('authToken', data.authToken);
         this.localStorage.store('username', data.username);
-        
+
         this.loggedIn.emit(true);
         this.username.emit(data.username);
+        this.isLoggedin = true;
         return true;
       }));
   }
+
+
+  getEmiter() {
+    return this.username;
+  }
+
+  logout() {
+    this.httpClient.post('logout', {}).subscribe(function () {
+      this.app.authenticated = false;
+
+    });
+    this.localStorage.clear('username');
+    this.localStorage.clear('authToken');
+    this.isLoggedin = false;
+
+  }
+
+
   getJwtToken() {
     return this.localStorage.retrieve('authToken');
   }
+
   isLoggedIn(): boolean {
     return this.getJwtToken() != null;
   }
+
   getUserName() {
     return this.localStorage.retrieve('username');
-  }
-  getRefreshToken() {
-    return this.localStorage.retrieve('refreshToken');
   }
 }
